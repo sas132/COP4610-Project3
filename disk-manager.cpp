c1d5f4f01c5ceab1d4ac64183bce0ab2132ca5a5
Sarah Stevenson <sas132@students.uwf.edu>
2020-11-12 13:50:46 -0600

    accepts input

--- disk-manager.cpp
+++ disk-manager.cpp
@@ -5,10 +5,17 @@
 
 DiskManager::DiskManager()
 {
-	int startLocation;
+	int startLocation = 0;
 
 	std::cout << "Please enter the current location of the arm: ";
-	std::cin >> startLocation;
+	
+	std::string str;
+	std::getline(std::cin, str);
+
+	for(int i = 0; i < str.size(); i++)
+	{
+		startLocation = (startLocation * 10) + ((int)str.at(i) - 48);
+	}
 	
 	Location* first = new Location(startLocation);
 	firstLocation = first;
@@ -22,6 +29,37 @@ void DiskManager::MakeLocations()
 	std::cout << "Please enter the list of the cylinder numbers for the file: ";
 	std::getline(std::cin, str);
 	int current = 0;
+	Location* lastLoc = new Location(-1);
 
-	std::cout << "\n\n" << firstLocation->getCylNum();
+	for(int i = 0; i < str.size(); i++)
+	{
+		if(isdigit(str.at(i)))
+		{
+			current = (current * 10) + ((int)str.at(i) - 48);
+		}
+		else if(str.at(i) == ' ')
+		{
+			if(lastLoc->getCylNum() == -1)
+			{
+				Location* temp = new Location(current);
+				firstLocation->setNext(temp);
+				delete lastLoc;
+				lastLoc = temp;
+			}
+			else
+			{
+				Location* temp = new Location(current);
+				lastLoc->setNext(temp);
+				lastLoc = temp;
+			}
+			current = 0;
+		}
+		else
+		{
+			std::cerr << "ERROR: Invalid Input.\n";
+		}
+	}
+
+	Location* temp = new Location(current);
+	lastLoc->setNext(temp);
 }
